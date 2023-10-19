259d03ce8ab7273b1b87cc5584b9500162e2e6ff
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-10-19 17:25:31 +0530

    Update Hero.js

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,6 +5,21 @@ const Hero = () => {
   return (
     <div className='hero_Section'>
        <Navbar/>
+       <div className="hero_Container">
+        <div className="left">
+          <h1>Think. Make . Solve</h1>
+          <div className="what_we_do">
+            <img src="./img/line.png" alt="" className="line" />
+           <h2 className='subtitle'>What we do</h2>
+          </div>
+           <p className='info'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum fugit porro 
+          ?</p>
+           <button>Learn More</button>
+        </div>
+        <div className="right">
+          <img src="./img/moon.png" alt="" />
+        </div>
+       </div>
     </div>
   )
 }
