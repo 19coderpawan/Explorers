d5e440f374a25d2e5ceb188adb4a4abc94dcfb83
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-10-20 23:46:01 +0530

    Update Who.js

--- src/Components/Who.js
+++ src/Components/Who.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 const Who = () => {
   return (
-    <div  className='who_Section'>
-      Who
+    <div  className='who_Section'>  
+       <div className="Who_Container">
+        <div className="left_who">
+          {/* 3D model  */}
+        </div>
+        <div className="right_who">
+          <h1>Think outside the square box</h1>
+          <div className="what_we_do">
+            <img src="./img/line.png" alt="" className="line" />
+           <h2 className='subtitle'>What we Work</h2>
+          </div>
+           <p className='info'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum fugit porro 
+          ?</p>
+           <button>See our Work</button>
+        </div>
+       </div>
     </div>
   )
 }
