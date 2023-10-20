1bd0198f18f34700a4620afc9333e97da0c1ba4a
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-10-20 23:46:27 +0530

    Update Work.js

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
 const Work = () => {
+  const data=[
+    "Web Design",
+    "Development",
+    "Illustration",
+    "Product Design",
+    "Social Media"
+  ]
   return (
-    <div  className='work_Section'>
-    Work
+    <div className='work_Section'>
+      <div className="work_container">
+        <div className="Work_left">
+          <ul className="datalist">
+            {data.map((list)=>(
+              <li key={list} style={{  '--text': 'web design'}}>{list}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="Work_right"></div>
+      </div>
     </div>
   )
 }
