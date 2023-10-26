06ef2d82baed8c6417ab9779211e931623a4a3b7
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-10-26 19:16:03 +0530

    Update Who.js

--- src/Components/Who.js
+++ src/Components/Who.js
@@ -1,11 +1,23 @@
 import React from 'react'
+// import Testthree from './Test_three'
+import {Canvas} from '@react-three/fiber'
+import { OrbitControls} from '@react-three/drei'
+import Treseaurerectcube from './treseaurerect_cube'
 
 const Who = () => {
   return (
     <div  className='who_Section'>  
        <div className="Who_Container">
         <div className="left_who">
-          {/* 3D model  */}
+          {/* there should be any other way to give the dimensions to the cube other than boxgeometry. */}
+        <Canvas camera={{fov:25, position:[5,5,5]}} >
+            <OrbitControls autoRotate={true}/>
+            {/* to include the color in the material */}
+            <ambientLight intensity={1}/>
+            {/* to see the edges of the material */}
+            <directionalLight position={[3,2,1]}/>
+            <Treseaurerectcube/>
+        </Canvas>
         </div>
         <div className="right_who">
           <h1>Think outside the square box</h1>
