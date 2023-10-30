ee5bdb3f868bb41a3d7dd9d86050a7ee0b338c7b
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-10-30 18:40:43 +0530

    Update WebDesign.js

--- src/Components/WebDesign.js
+++ src/Components/WebDesign.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { Model } from './Scene1'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, PerspectiveCamera, Stage } from '@react-three/drei'
+import styled from 'styled-components'
+
+const Web=styled.div`
+height:100vh;
+scroll-snap-align:center;
+display:flex;
+justify-content:center;
+`
+const Container=styled.div`
+ width:1400px;
+  height: 100vh;
+  display:flex;
+  justify-content:space-between;
+`
 
 const WebDesign = () => {
   return (
-    <div>
-      webdesign
-    </div>
+    <Web>
+      <Container>
+
+    <Canvas>
+      
+      {/* <ambientLight intensity={2}/>
+      <directionalLight position={[1,1,1]}/> */}
+      
+        <Stage environment="park" intensity={1} >
+        <Model/>
+        </Stage>
+        <OrbitControls autoRotate scale={4}  />    
+        
+     
+    </Canvas>
+      </Container>
+    </Web>
   )
 }
 
