674396da143311ea9a0c69d3d1ba99e4cd1f2013
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-10-30 18:41:05 +0530

    Update Development.js

--- src/Components/Development.js
+++ src/Components/Development.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Model } from './Scene2'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, PerspectiveCamera, Stage } from '@react-three/drei'
+import styled from 'styled-components'
+
+const Dev=styled.div`
+height: 100vh;
+scroll-snap-align: center;
+display: flex;
+justify-content: center;
+
+`
+const Container=styled.div`
+ width:1400px;
+  height: 100vh;
+  display:flex;
+  justify-content:space-between;
+`
 
 const Development = () => {
   return (
-    <div>
-      Development
-    </div>
+    <Dev>
+      <Container>
+        <Canvas>
+          <OrbitControls autoRotate/>
+          <Stage environment="city" intensity={1}>
+            <Model/>
+          </Stage>
+        </Canvas>
+      </Container>
+    </Dev>
   )
 }
 
