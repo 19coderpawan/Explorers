ffcb01750e44afd6ae1675ce58335c1139aca49f
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-10-31 19:54:46 +0530

    Update WebDesign.js

--- src/Components/WebDesign.js
+++ src/Components/WebDesign.js
@@ -6,36 +6,48 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls, PerspectiveCamera, Stage } from '@react-three/drei'
 import styled from 'styled-components'
 
-const Web=styled.div`
+const Web = styled.div`
 height:100vh;
 scroll-snap-align:center;
 display:flex;
 justify-content:center;
 `
-const Container=styled.div`
+const Container = styled.div`
  width:1400px;
   height: 100vh;
   display:flex;
   justify-content:space-between;
+  position: relative;
+`
+const Desc=styled.p`
+width: 200px;
+height: 70px;
+background-color: white;
+color: black;
+/* position: absolute; */
+top:100px;
+left:100px;
+border-radius:10px;
+padding:10px;
 `
 
 const WebDesign = () => {
   return (
     <Web>
       <Container>
+       <Desc>Captain America  is one of the strongest avengers.</Desc>
+        <Canvas>
 
-    <Canvas>
-      
-      {/* <ambientLight intensity={2}/>
+          {/* <ambientLight intensity={2}/>
       <directionalLight position={[1,1,1]}/> */}
-      
-        <Stage environment="park" intensity={1} >
-        <Model/>
-        </Stage>
-        <OrbitControls autoRotate scale={4}  />    
-        
-     
-    </Canvas>
+
+          <Stage environment="park" intensity={1} >
+            <Model />
+          </Stage>
+          <OrbitControls autoRotate scale={4} />
+
+
+        </Canvas>
       </Container>
     </Web>
   )
