411143103d29ebbbe59a266c8df33826df46a4b5
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-11-05 18:32:24 +0530

    Update Scene1.js

--- src/Components/Scene1.js
+++ src/Components/Scene1.js
@@ -7,7 +7,7 @@ Source: https://sketchfab.com/3d-models/avengers-endgame-df0ab2d8550b430e96819e1
 Title: Avengers - Endgame
 */
 
-import React, { useRef } from 'react'
+import React from 'react'
 import { useGLTF } from '@react-three/drei'
 
 export function Model(props) {
