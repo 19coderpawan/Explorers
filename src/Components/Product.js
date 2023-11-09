f9c092958b1932a6556a7ca9c18bdc0218cbbd25
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2023-11-10 01:08:24 +0530

    Responsiveness

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -12,21 +12,43 @@ justify-content: center;
 
 `
 const Container=styled.div`
- width:1400px;
+ width: 100%;
+  max-width: 1600px;
   height: 100vh;
-  display:flex;
-  justify-content:space-between;
+  display: flex;
+  flex-direction: column-reverse; /* Reverse the order for smaller screens */
+  justify-content: space-between;
+  @media only screen and (max-width: 786px) {
+    flex-direction: column; /* Change to column layout for smaller screens */
+    align-items: center;
+    justify-content: center;
+    /* height: 50%; */
+    /* max-width: 2000px; */
+    /* border:none; */
+  }
 `
 const Desc=styled.p`
-width: 200px;
-height: 70px;
-background-color: white;
-color: black;
-/* position: absolute; */
-bottom:100px;
-left:100px;
-border-radius:10px;
-padding:10px;
+ width: 100%;
+  max-width: 700px;
+  height: 90px;
+  background-color: white;
+  color: black;
+  border-radius: 10px;
+  padding: 10px;
+  margin: 30px; /* Add margin for better spacing */
+  overflow: hidden;
+  @media only screen and (max-width: 786px) {
+    padding:30px;
+    font-size:15px;
+    /* width:100%; */
+    height: 40px;
+    border-radius: 30%;
+    /* max-width: 900px; */
+    text-align: center;
+  }
+`
+const Canvascontainer= styled.div`
+ flex-grow: 3;
 `
 
 const Product = () => {
@@ -36,12 +58,14 @@ const Product = () => {
         <Desc>
           Thanos is one of the Strongest Villans in the MCU.
         </Desc>
+        <Canvascontainer>
         <Canvas>
           <OrbitControls autoRotate/>
           <Stage environment="city" intensity={1}>
             <Model/>
           </Stage>
         </Canvas>
+        </Canvascontainer>
       </Container>
     </Thanos>
   )
